51be3212c1ebe061b09cdcb72eded093b7ce8d85
Micah Smith <86262666+MicahWSmith@users.noreply.github.com>
2021-12-13 21:40:56 -0500

    setup for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecommenderComponent } from './recommender/recommender.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: "recommender", component: RecommenderComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
